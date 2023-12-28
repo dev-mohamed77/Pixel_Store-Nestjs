391f738eb1b975d7ebb7c1f54c90a86ec254466e
dev-mohamed77 <developer.mohamed55@gmail.com>
2023-12-28 02:33:37 +0200

    init i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,31 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './application/common/database/database.module';
 import { AuthModule } from './presentation/auth/auth.module';
 import { UserModule } from './presentation/user/user.module';
+import {
+  AcceptLanguageResolver,
+  HeaderResolver,
+  I18nModule,
+  QueryResolver,
+} from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+        logging: true,
+        resolvers: [
+          { use: QueryResolver, options: ['lang'] },
+          AcceptLanguageResolver,
+          new HeaderResolver(['x-lang']),
+        ],
+      },
     }),
     DatabaseModule,
     AuthModule,
